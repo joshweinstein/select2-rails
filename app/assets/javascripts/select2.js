0ee7b0e9249b03ce1afc6c8df9c79aafa9b5b788
joshweinstein <weinstein.josh@gmail.com>
2013-08-28 18:14:00 +0700

    Removed 'createSearchChoice' from the array on ln. 840 of select2.js so that we can populate the <select> with a normal rails helper, and also have select2 let the user create a new customer from the name she typed into the select field.

--- app/assets/javascripts/select2.js
+++ app/assets/javascripts/select2.js
@@ -837,7 +837,7 @@ the specific language governing permissions and limitations under the Apache Lic
 
             if (select) {
                 // these options are not allowed when attached to a select because they are picked up off the element itself
-                $.each(["id", "multiple", "ajax", "query", "createSearchChoice", "initSelection", "data", "tags"], function () {
+                $.each(["id", "multiple", "ajax", "query", "initSelection", "data", "tags"], function () {
                     if (this in opts) {
                         throw new Error("Option '" + this + "' is not allowed for Select2 when attached to a <select> element.");
                     }
